5de1cfa085276a3ffba856ef6a4f50992ef8f04f
gofogo <promoted.go@gmail.com>
2024-04-08 15:17:02 +0900

    구조변경 지역 -> 대륙

--- app/enterance/loading.tsx
+++ app/enterance/loading.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const loading = () => {
-  return <div className="flex items-center justify-center w-screen h-screen text-4xl font-extrabold" >Loading...</div>;
+  return <div>Loading...</div>;
 };
 
 export default loading;
